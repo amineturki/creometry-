1c4a9943e8741beb302263e3e0958d3d7a6c0de1
amineturki <58289763+amineturki@users.noreply.github.com>
2022-06-17 17:58:07 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -17,7 +17,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.gcp_account_json)
+ # credentials = file(var.gcp_account_json)
   project     = var.gcp_project
   region      = var.gcp_region
   zone        = var.gcp_zone
